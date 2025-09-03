ae1facab3d3a212ac3c107a84ba3eee319d3c661
Femi Adegbite <=>
2025-09-03 13:09:48 -0600

    login user and handle session

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,37 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
 import { ThemeToggle } from "@/components/ui/theme-toggle";
-import Image from "next/image";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 export default function Home() {
+  const router = useRouter();
+  const { data: session } = authClient.useSession();
+
+  const signout = async () => {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/");
+          toast.success('Sign out successfully')
+        },
+      },
+    });
+  };
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       Hello world
-
-      <ThemeToggle/>
+      <ThemeToggle />
+      {session ? (
+        <div>
+          <p>{session.user.name}</p>
+          <Button onClick={signout}>Logout</Button>
+        </div>
+      ) : (
+        <Button>Login</Button>
+      )}
     </div>
   );
 }
